b2a9466774d85e0374fb1fa2ea9a7a1efa0ea217
Zaning <61484146+zaning424@users.noreply.github.com>
2020-02-25 22:07:27 -0500

    Create HW2.py

---
+++ HW2.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_source(url):
+    company_dic = []
+    for i in range(0, 50):
+        get_resp = requests.get(url)
+        web_content = get_resp.content
+        soup = BeautifulSoup(web_content, "html.parser")
+        body_list = soup.select('li')
+        temp_list = []
+        for elem in body_list:
+            if elem is None:
+                continue
+            elem = str(elem).replace('<li>', '')
+            elem = str(elem).replace('</li>', '')
+            if 'Name' in elem:
+                temp_list.append(elem)
+                continue
+            if 'Purpose' in elem:
+                temp_list.append(elem)
+                continue
+        company_dic.append(temp_list)
+    return company_dic
+
+company_list = get_source("http://18.207.92.139:8000/random_company")
+
+
+def file_generate(list):
+    file1 = open('595_HW2.txt', 'w')
+    for ele in list:
+        for sub_ele in ele:
+            file1.write(sub_ele)
+            file1.write('\n')
+        file1.write('\n')
+
+    file1.close()
+
+file_generate(company_list)
